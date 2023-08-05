69d81f1821b12898a3453d559174ed21debef72d
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2023-08-05 23:10:27 +0000

    New approach with Dockerfile

--- .devcontainer/library-scripts/install-flutter.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.10.6-stable.tar.xz -O /tmp/flutter.tar.xz
-tar -xf /tmp/flutter.tar.xz -C $HOME
